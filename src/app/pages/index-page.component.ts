8065b4299ad685b743500ddd14af9410050ee8a7
Char2s <Char2s@outlook.com>
2022-04-02 12:14:08 +0800

    feat(client): translate the start up page

--- src/app/pages/index-page.component.ts
+++ src/app/pages/index-page.component.ts
@@ -7,20 +7,20 @@ import { Component } from '@angular/core';
                 <img class="bg" src="/assets/images/startpage-bg.svg"/>
             </div>
             <div class="wrapper text">
-                <div class="overline fadeIn" style="--delay: 1s">INTRODUCING</div>
+                <div class="overline fadeIn" style="--delay: 1s">登场</div>
 
-                <h1 class="fadeIn" style="--delay: 1s">HIGH PERFORMANCE<br/>TYPESCRIPT FRAMEWORK</h1>
+                <h1 class="fadeIn" style="--delay: 1s">高性能<br/>TYPESCRIPT 框架</h1>
             </div>
 
             <main>
-                <div class="overline fadeIn" style="--delay: 2s">ALPHA</div>
+                <div class="overline fadeIn" style="--delay: 2s">主旨</div>
                 <p class="tagline fadeIn" style="--delay: 2.5s">
-                    High-quality TypeScript libraries and a framework that brings everything together.<br/>
-                    Precisely aligned, gently optimised, solid engineered.
+                    高性能的 TypeScript 库，以及一个将所有东西聚合在一起的框架。<br/>
+                    精确的集成，优雅的优化，坚实的工程。
                 </p>
                 <p class="buttons fadeIn" style="--delay: 3s">
-                    <a class="button big" routerLink="/framework">Framework</a>
-                    <a class="button big" routerLink="/library">Libraries</a>
+                    <a class="button big" routerLink="/framework">框架</a>
+                    <a class="button big" routerLink="/library">库</a>
                 </p>
             </main>
             
